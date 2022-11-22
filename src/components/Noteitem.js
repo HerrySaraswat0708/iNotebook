a939ad38e4b3039c55cc83089d2f2a18f9e8fc6a
HerrySaraswat0708 <harrysaraswat3@gmail.com>
2022-11-22 17:26:40 +0530

    seventh

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -12,12 +12,12 @@ const Noteitem = (props) => {
         <div className="card-body">
           <h5 className="card-title">{note.title}</h5>
           <p className="card-text">{note.description}</p>
-          <span className="mx-2">
+          {/* <span className="mx-2">
             <i className="fa fa-pencil-square-o" area-hidden="true" onClick={()=>{updateNote(note)}} />
           </span>
           <span className="mx-3">
             <i className="fa fa-trash" area-hidden="true" onClick={()=>{deleteNote(note._id)}} />
-          </span>
+          </span> */}
         </div>
       </div>
     </div>
